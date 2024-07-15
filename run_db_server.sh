006a3f2b4ba4cab8e878564b6ead54cd8ba3866b
niccoreyes <niccoreyes@gmail.com>
2024-07-15 20:39:17 +0800

    refactor: Improve port management and process handling
    - Removed unnecessary line printing directory
    - Added `check_port_kill` function to identify and kill processes using specific ports
    - Stopped Caddy before checking and killing ports 3000, 5173, 8103, and 2019
    - Stopped Caddy service using `sudo caddy stop` `sudo systemctl stop caddy`
    - Navigated to the parent directory for Caddyfile

--- run_db_server.sh
+++ run_db_server.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-# Debug the current directory
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-echo "Current script directory: $SCRIPT_DIR"
+# Function to check and kill a specific port
+check_port_kill() {
+    local port="$1"
+
+    # Check if anything is running on the specified port
+    if sudo lsof -i :"$port"; then
+        # Get the PID of the process using the port
+        local pid=$(sudo lsof -t -i :"$port")
+        echo "Process with PID $pid is using port $port. Killing it..."
+        sudo kill -15 "$pid"
+    fi
+}
+
+# stop Caddy first
+sudo caddy stop
+
+check_port_kill 3000
+check_port_kill 5173
+check_port_kill 8103
+check_port_kill 2019
 
 # run Medplum FHIR server
 cd packages/server
-npm run dev &
+npm run dev&
 
 # run Medplum App react
 cd ../app
-npm run dev -- --host &
+npm run dev&
 
 # Run Caddy
-cd ../../
+sudo systemctl stop caddy
+cd ../../../
 sudo caddy run
